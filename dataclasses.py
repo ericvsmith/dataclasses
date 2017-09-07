824d4012c17cc2794e67a9e1a56f6da8b7fb6777
Eric V. Smith <eric@trueblade.com>
2017-09-07 14:20:06 -0700

    Change the paramaters to `dataclass` to move parameters which are more likely to be used to the front. Again, it doesn't really matter because they're all keyword only.

--- dataclasses.py
+++ dataclasses.py
@@ -429,7 +429,7 @@ def _process_class(cls, repr, cmp, hash, init, frozen):
 # _cls should never be specified by keyword, so start it with an
 #  underscore. The presense of _cls is used to detect if this
 #  decorator is being called with parameters or not.
-def dataclass(_cls=None, *, repr=True, cmp=True, hash=None, init=True,
+def dataclass(_cls=None, *, init=True, repr=True, hash=None, cmp=True,
                frozen=False):
     def wrap(cls):
         return _process_class(cls, repr, cmp, hash, init, frozen)
