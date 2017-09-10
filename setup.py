ec0e8833391ca38999ae4ec51e6f70ec12df2b18
Eric V. Smith <eric@trueblade.com>
2017-09-10 19:51:30 -0400

    Add setup.py and files needed to upload to PyPI. Tag version 0.1.

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+setup(
+    name='dataclasses',
+    version='0.1',
+    description='An implementation of PEP 557: Data Classes',
+    url='https://github.com/ericvsmith/dataclasses',
+    author='Eric V. Smith',
+    author_email='eric@python.org',
+    license='MIT',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+    ],
+    py_modules=['dataclasses']
+)
