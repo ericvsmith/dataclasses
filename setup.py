bced91e9b25c71ffd86038c457ab02455ceed58e
Eric V. Smith <eric@trueblade.com>
2018-01-27 21:15:45 -0500

    Bump version, remove 3.7 label, change to Beta.

--- setup.py
+++ setup.py
@@ -2,19 +2,18 @@ from setuptools import setup
 
 setup(
     name='dataclasses',
-    version='0.3',
+    version='0.4',
     description='An implementation of PEP 557: Data Classes',
     url='https://github.com/ericvsmith/dataclasses',
     author='Eric V. Smith',
     author_email='eric@python.org',
     license='MIT',
     classifiers=[
-        'Development Status :: 3 - Alpha',
+        'Development Status :: 4 - Beta',
         'Intended Audience :: Developers',
         'Topic :: Software Development :: Libraries :: Python Modules',
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
     ],
     py_modules=['dataclasses']
 )
