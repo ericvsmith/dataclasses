b36d829bdeb2437c1f870df9f3b36e7f7f907a5e
Eric V. Smith <eric@trueblade.com>
2018-04-19 05:33:04 -0400

    Change license to Apache 2.0. Add LICENSE.txt and copyright language.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='dataclasses',
-    version='0.5',
+    version='0.5+',
     description='An implementation of PEP 557: Data Classes',
     url='https://github.com/ericvsmith/dataclasses',
     author='Eric V. Smith',
@@ -12,7 +12,7 @@ setup(
         'Development Status :: 4 - Beta',
         'Intended Audience :: Developers',
         'Topic :: Software Development :: Libraries :: Python Modules',
-        'License :: OSI Approved :: MIT License',
+        'License :: OSI Approved :: Apache Software License',
         'Programming Language :: Python :: 3.6',
     ],
     py_modules=['dataclasses']
