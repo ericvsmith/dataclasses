95d2a4ca30e9dfdbc6837921cc5a56b89364be62
Eric V. Smith <eric@trueblade.com>
2020-11-13 09:26:05 -0500

    Prepare for 0.8 release.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.rst") as readme:
 
 setup(
     name="dataclasses",
-    version="0.7",
+    version="0.8",
     description="A backport of the dataclasses module for Python 3.6",
     long_description=README,
     url="https://github.com/ericvsmith/dataclasses",
